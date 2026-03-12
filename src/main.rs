f238a077c86c14e1e01ac58f8df8a2187a1717fc
YusufErdemK <yusuferdemyek@gmail.com>
2026-03-12 18:49:51 +0300

    main: parse html
    Signed-off-by: YusufErdemK <yusuferdemyek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
+use scraper::{Html, Selector};
+
 fn main() {
     let url = "https://example.com";
     let body = reqwest::blocking::get(url)
-        .expect("istek başarısız:(")
+        .expect("Request başarısız")
         .text()
-        .expect("bunasılbodyaq");
-    
-    println!("{}", body);
+        .expect("Body okunamadı");
+
+    let document = Html::parse_document(&body);
+
+    let h1 = Selector::parse("h1").unwrap();
+    for element in document.select(&h1) {
+        println!("H1: {}", element.text().collect::<String>());
+    }
+
+    let p = Selector::parse("p").unwrap();
+    for element in document.select(&p) {
+        println!("P: {}", element.text().collect::<String>());
+    }
 }
