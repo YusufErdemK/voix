fdbcbee18fed68456ad403e21c7cdb0e44a2ef57
YusufErdemK <yusuferdemyek@gmail.com>
2026-03-12 19:13:10 +0300

    main: new features
    Signed-off-by: YusufErdemK <yusuferdemyek@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Box, Button, Entry, Orientation};
-use webkit2gtk::{WebView, WebViewExt};
+use gtk::{Application, ApplicationWindow, Box, Button, Entry, Orientation, Spinner};
+use webkit2gtk::{LoadEvent, WebView, WebViewExt};
+
+fn navigate(webview: &WebView, input: &str) {
+    let url = if input.starts_with("http://") || input.starts_with("https://") {
+        input.to_string()
+    } else if input.contains('.') && !input.contains(' ') {
+        format!("https://{}", input)
+    } else {
+        format!("https://www.google.com/search?q={}", input.replace(' ', "+"))
+    };
+    webview.load_uri(&url);
+}
 
 fn main() {
     let app = Application::builder()
@@ -18,41 +29,72 @@ fn main() {
         let vbox = Box::new(Orientation::Vertical, 0);
         let hbox = Box::new(Orientation::Horizontal, 4);
 
-        let url_bar = Entry::new();
-        url_bar.set_placeholder_text(Some("example.com"));
-        url_bar.set_hexpand(true);
+        let back_btn    = Button::with_label("◀");
+        let forward_btn = Button::with_label("▶");
+        let reload_btn  = Button::with_label("↺");
+        let url_bar     = Entry::new();
+        let spinner     = Spinner::new();
 
-        let go_btn = Button::with_label("Git");
+        url_bar.set_placeholder_text(Some("Ara veya adres yaz..."));
+        url_bar.set_hexpand(true);
+        back_btn.set_sensitive(false);
+        forward_btn.set_sensitive(false);
 
         let webview = WebView::new();
         webview.set_vexpand(true);
         webview.load_uri("https://www.google.com");
 
-        // Enter
-        let wv1 = webview.clone();
+        let wv = webview.clone();
+        back_btn.connect_clicked(move |_| { wv.go_back(); });
+
+        let wv = webview.clone();
+        forward_btn.connect_clicked(move |_| { wv.go_forward(); });
+
+        let wv = webview.clone();
+        reload_btn.connect_clicked(move |_| { wv.reload(); });
+
+        let wv = webview.clone();
         url_bar.connect_activate(move |entry| {
-            let mut url = entry.text().to_string();
-            if !url.starts_with("http://") && !url.starts_with("https://") {
-                url = format!("https://{}", url);
-            }
-            wv1.load_uri(&url);
+            navigate(&wv, &entry.text());
         });
 
-        // Buton
-        let wv2 = webview.clone();
-        let ub2 = url_bar.clone();
-        go_btn.connect_clicked(move |_| {
-            let mut url = ub2.text().to_string();
-            if !url.starts_with("http://") && !url.starts_with("https://") {
-                url = format!("https://{}", url);
+        let ub   = url_bar.clone();
+        let sp   = spinner.clone();
+        let back = back_btn.clone();
+        let fwd  = forward_btn.clone();
+        webview.connect_load_changed(move |wv, event| {
+            match event {
+                LoadEvent::Started   => { sp.start(); }
+                LoadEvent::Committed => {
+                    if let Some(uri) = wv.uri() { ub.set_text(&uri); }
+                }
+                LoadEvent::Finished  => {
+                    sp.stop();
+                    back.set_sensitive(wv.can_go_back());
+                    fwd.set_sensitive(wv.can_go_forward());
+                }
+                _ => {}
             }
-            wv2.load_uri(&url);
         });
 
-        hbox.pack_start(&url_bar, true, true, 4);
-        hbox.pack_start(&go_btn, false, false, 4);
-        vbox.pack_start(&hbox, false, false, 4);
-        vbox.pack_start(&webview, true, true, 0);
+        let ub = url_bar.clone();
+        window.connect_key_press_event(move |_, key| {
+            if key.state().contains(gtk::gdk::ModifierType::CONTROL_MASK)
+                && key.keyval() == gtk::gdk::keys::constants::l
+            {
+                ub.grab_focus();
+                return true.into();
+            }
+            false.into()
+        });
+
+        hbox.pack_start(&back_btn,    false, false, 2);
+        hbox.pack_start(&forward_btn, false, false, 2);
+        hbox.pack_start(&reload_btn,  false, false, 2);
+        hbox.pack_start(&url_bar,     true,  true,  4);
+        hbox.pack_start(&spinner,     false, false, 4);
+        vbox.pack_start(&hbox,    false, false, 4);
+        vbox.pack_start(&webview, true,  true,  0);
         window.add(&vbox);
         window.show_all();
     });
